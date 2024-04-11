1fce19f26ab09814520816ef65689395c8ed1603
maru pereira <martixana@gmail.com>
2024-04-11 13:10:57 +0100

    jbljb

--- FichaPratica08/src/Exercicio_02/Cao.java
+++ FichaPratica08/src/Exercicio_02/Cao.java
@@ -1,4 +1,25 @@
 package Exercicio_02;
 
+//atributos
 public class Cao {
-}
+
+    private String nome;
+
+    private String raca;
+
+    private String latido = "au au au";
+
+    //Construtor
+    public Cao(String nome, String raca){
+        this.nome;
+        this.raca;
+    }
+
+    //Método
+    public void  oMeuCao(){
+
+            System.out.println("O nome do meu cao é " + this.nome + "e é um " + this.raca);
+
+        };
+    }
+
