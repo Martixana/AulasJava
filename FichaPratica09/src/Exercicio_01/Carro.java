83d7cdb72d70298a837e85bd90ddff81d067d47b
maru pereira <martixana@gmail.com>
2024-04-16 10:35:56 +0100

    FichaPratica09

---
+++ FichaPratica09/src/Exercicio_01/Carro.java
@@ -0,0 +1,24 @@
+package Exercicio_01;
+
+public class Carro {
+
+    //attributes
+
+    private String marca;
+    private String modelo;
+    private int ano;
+    private int potencia;
+    private int cilindrada;
+    private tipoCombustivel combustivel;
+    private double litros100km;
+
+    //Constructor
+    public Carro (String marca, String modelo, int ano, int potencia, int cilindrada, tipoCombustivel combustivel,  double litros100km ){
+
+    }
+
+
+
+
+
+}
