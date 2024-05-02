631fb4c525a4ac69d9c4dacbf11091a19dd56f13
Martixana <martixana@gmail.com>
2024-05-02 23:18:59 +0100

    update

--- FichasPraticasJava/src/ficha_06alt/Ex_07.java
+++ FichasPraticasJava/src/ficha_06alt/Ex_07.java
@@ -3,62 +3,68 @@ package ficha_06alt;
 import java.util.Scanner;
 public class Ex_07 {
 
-    //function to print out first line
-    public static String drawSquare(int rows, int columns, String letter) {
+
+    public static void drawSquare(int rows, int columns, String letter) {
 
         //first line
         for (int m = 1; m <= columns; m++) {
-            for(int i = 1; i <= rows ; i++) {
                 System.out.print(letter);
             }
+
+        //empty line
+        System.out.println();
+
+        //middle lines  - need to write out as if line in matrix
+        for(int k = 1; k <= rows -2; k++){
+            for(int j = 1; j <= columns; j++) {
+                //first character
+                if (j == 1) {
+                    System.out.println(letter);
+                }
+                //middle characters
+                if (j != 1 && j != columns) {
+                    System.out.println(" ");
+                }
+
+                //last character
+                if (j == columns) {
+                    System.out.print(letter);
+                }
         }
-        return letter;
+        //empty line
+        System.out.println();
     }
-
-
-        //get number of columns from user
-        public static int numColumns(){
-            Scanner input = new Scanner(System.in);
-            int cols;
-
-            System.out.println("Please enter the number of columns:");
-            cols = input.nextInt();
-            return cols;
-        }
-
-
-
-        //get number of rows from User
-        public static int numRows(){
-            Scanner input = new Scanner(System.in);
-            int rows;
-
-            System.out.println("Please enter the number of rows:");
-            rows = input.nextInt();
-            return rows;
-        }
-
-
-
-        //get letters from user
-        public static String userLetter(){
-            Scanner input = new Scanner(System.in);
-            String letter;
-
-            System.out.println("Please enter a letter:");
-            letter = input.next();
-            return letter;
-        }
+                //last line
+                for(int i = 1; i <= columns; i++){
+                    System.out.print(letter);
+                }
+    }
 
 
         //main method
         public static void main(String[] args){
 
-            drawSquare(numRows(), numColumns(), userLetter());
+        Scanner input = new Scanner(System.in);
+
+        //variables
+         int rows, columns;
+         String letter;
+
+         //get number of rows from user
+         System.out.println("Please input a number for rows: ");
+         rows = input.nextInt();
+
+         //get number of columns from user
+         System.out.println("Please input a number for columns: ");
+         columns = input.nextInt();
+
+         //get letter from user
+         System.out.println("Please choose a letter from a to z:");
+         letter = input.next();
+
+         drawSquare(rows, columns, letter);
 
         }
-
-
     }
 
 
