769b059fa13b41e57a2e6a738d9de105c400b8e8
Martixana <martixana@gmail.com>
2024-05-04 19:35:29 +0100

    create ex 8

---
+++ FichasPraticasJava/src/ficha_06alt/Ex_08.java
@@ -0,0 +1,27 @@
+package ficha_06alt;
+
+//import java.util.Scanner;
+public class Ex_08 {
+
+    public static void somarMatrizes(int[][] matrixA, int[][] maatrixB){
+
+        int somatorio = 0;
+
+        //loop through matrixA and add elements
+        for(int i = 0; i < matrixA.length; i++){
+
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+
+        int[][] matrixA = new int[3][3];
+        int[][] matrixB = new int[3][3];
+
+        
+
+
+    }
+}
