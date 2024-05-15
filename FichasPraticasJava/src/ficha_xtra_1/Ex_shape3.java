2f565e330c956e8572b7e0100fbc0c5b0e2ea6d2
Martixana <martixana@gmail.com>
2024-05-16 00:06:07 +0100

    update

--- FichasPraticasJava/src/ficha_xtra_1/Ex_shape3.java
+++ FichasPraticasJava/src/ficha_xtra_1/Ex_shape3.java
@@ -5,9 +5,12 @@ public class Ex_shape3 {
         //variable
         int rows = 8;
 
-        for(int i = 0; i < rows; i++){
-            for(int k = rows; k > i; k-- ){
-                System.out.print(" * ");
+        for(int i = 0; i <= rows; i++){
+            for(int k = 1; k <= rows - i; k++ ){
+                System.out.print(" ");//creating the blanks
+            }
+            for(int m = 0; m <= i; m++ ){
+                System.out.print("*");
             }
             System.out.println();
         }
@@ -16,4 +19,4 @@ public class Ex_shape3 {
 
 }
 
-}
+
